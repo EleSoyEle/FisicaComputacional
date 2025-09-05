87ab8b1139883c90a1b33bc7f06272970786a2d6
Angel <angelgvsgm@gmail.com>
2025-09-04 20:02:07 -0600

    Añadiendo el histograma

--- Box-Muller/script.py
+++ Box-Muller/script.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def MakeRandomNormalNumbers(mean,points):
+    #Utilizamos arrays para almacenar los numeros generados
     z_0 = []
     z_1 = []
     for i in range(points):
@@ -13,7 +14,21 @@ def MakeRandomNormalNumbers(mean,points):
         R = math.sqrt(-2*math.log(u1))
         z_0.append(R*math.cos(2*math.pi*u2)+mean)
         z_1.append(R*math.sin(2*math.pi*u2)+mean)
-    return z_0,z_1
+    return z_0 #,z_1
 
 
-print(MakeRandomNormalNumbers(0,100))
+#Media
+m = 0 
+#Numero de puntos
+points = 10000
+
+#Numero de columnas
+columns = int(np.sqrt(points))
+
+random_numb = MakeRandomNormalNumbers(m,points)
+plt.grid()
+plt.hist(random_numb,columns,edgecolor="black")
+plt.xlabel("Valor en el eje x")
+plt.ylabel("Frecuencia")
+plt.title("Histograma de los números generados")
+plt.show()
