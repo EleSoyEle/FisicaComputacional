5effdf11332a35b417deaa58a1903d932ecd7c6f
Angel <angelgvsgm@gmail.com>
2025-09-25 21:37:26 -0600

    Version final final verdadero(no mostrar al cliente)

--- Laboratorio_1b/script.py
+++ Laboratorio_1b/script.py
@@ -5,7 +5,7 @@ from scipy.integrate import quad
 
 cmap = plt.cm.get_cmap("magma")
 
-def integrar_aleatorio(func,num_points,a,b,mean,std):
+def integrar_aleatorio(title,func,num_points,a,b,mean,std):
     #Generamos los puntos aleatorios y evaluamos
     random_points = np.random.normal(mean,std,num_points)
     random_points_d = []
@@ -19,13 +19,16 @@ def integrar_aleatorio(func,num_points,a,b,mean,std):
 
     integral_real = quad(func,a,b)   
     print('''
+
     
+    {}
+    ------------------------------
     Valor real: {}
     Valor estimado: {}
-
-    '''.format(integral_real[0],prom)) 
+    ------------------------------
+    '''.format(title,integral_real[0],prom)) 
     sp = np.linspace(a,b,1000)
-    plt.title("Gráfica de la función")
+    plt.title(title)
     plt.xlabel("Eje x")
     plt.ylabel("Eje y")
     plt.grid()
@@ -35,10 +38,18 @@ def integrar_aleatorio(func,num_points,a,b,mean,std):
     colors = [cmap(n_scale[i]) for i in range(len(scale))]
     plt.scatter(random_points_d,-1*np.ones_like(random_points_d),s=scale,c=colors,label="Puntos aleatorios")
     plt.legend()
+    plt.savefig("{}.png".format(title))
     plt.show()
     
     return prom
 
 num_points = 1000
-f = lambda x:10*np.exp(-5*(x-3)**4)
-integrar_aleatorio(f,10000,-10,10,3,1)
+funcs = [
+    [lambda x:10*np.exp(-5*(x-3)**4),-20,20,3,1],
+    [lambda x:1/np.sqrt(2*np.pi)*np.exp(-x**2/2),1,3,2,0.1],
+    [lambda x:1/np.sqrt(2*np.pi)*np.exp(-x**2/2),3,20,2,1],
+    [lambda x:np.exp(-(x-6)**2/2),-20,20,6,1]
+]
+
+for i,elem in enumerate(funcs):
+    integrar_aleatorio("Integral {}".format(i+1),elem[0],num_points,elem[1],elem[2],elem[3],elem[4])
