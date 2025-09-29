cbba14b99308b7583f20625af6e21478c9f27e9f
Angel <angelgvsgm@gmail.com>
2025-09-29 10:26:02 -0600

    Jijijaja

--- DinamicaMolecular/script.py
+++ DinamicaMolecular/script.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-N = 10
+N = 20
 
 #Tambien va a ayudarnos a delimitar el tamaño de la caja
-min_g = -10
-max_g = 10
+min_g = -50
+max_g = 50
 
-v_p = 0
+v_p = 5
 std = 0.5
 
 parts = np.array([ np.random.uniform(min_g+1,max_g-1,2) for i in range(N)
@@ -27,10 +27,10 @@ hist_p = [
 hist_v = [
     v_parts
 ]
-mp = 1e-4
+mp = 1
 kb = 1
 
-Temp = [mp*np.mean(v_parts)**2/kb]
+Temp = [mp*np.mean(v_parts)**2/(N*kb)]
 
 #Parametros del potencial de Lennard-Jones
 sigma = 1
@@ -38,18 +38,14 @@ epsilon = 1
 def CalcF(pos0,pos,idx=None):
     fx = 0
     fy = 0
-    qp = 1
-    if idx==None:
-        qp=1
-    else:
-        qp = q[idx]
     for im,p in enumerate(pos):
-        if not (p[0]==pos0[0] and p[1]==pos0[1]):
+        if not idx==im:
             r = ((pos0[0]-p[0])**2+(pos0[1]-p[1])**2)**(1/2)
-            fp = 4*epsilon*(12*np.power(sigma/r,13)-6*np.power(sigma/r,6))
-            
-            fx = fp*(pos0[0]-p[0])/r
-            fy = fp*(pos0[1]-p[1])/r
+            if not r==np.zeros_like(r):
+                fp = 4*epsilon/sigma*(12*np.power(sigma/r,13)-6*np.power(sigma/r,6))
+
+                fx = fp*(pos0[0]-p[0])/r
+                fy = fp*(pos0[1]-p[1])/r
     return fx,fy
 
 dt = 0.1
@@ -60,12 +56,16 @@ def MRU_step():
     for i in range(len(parts)):
         vn_part_i = np.copy(hist_v[-1][i])
         fx,fy = CalcF(hist_p[-1][i],hist_p[-1],i)
-        vn_part_i[0] = hist_v[-1][i][0]+fx*dt
-        vn_part_i[1] = hist_v[-1][i][1]+fy*dt
 
         pn_part_i = np.copy(hist_p[-1][i])
-        pn_part_i[0] += vn_part_i[0]*dt
-        pn_part_i[1] += vn_part_i[1]*dt
+        pn_part_i[0] += vn_part_i[0]*dt + fx*dt**2/(2*mp)
+        pn_part_i[1] += vn_part_i[1]*dt + fy*dt**2/(2*mp)
+
+        fxn,fyn = CalcF(pn_part_i,hist_p[-1],i)
+
+        vn_part_i[0] += (fxn+fx)*dt/(2*mp)
+        vn_part_i[1] += (fyn+fy)*dt/(2*mp)
+
 
         if min_g > pn_part_i[0]:
             pn_part_i[0] = max_g-1
@@ -103,7 +103,6 @@ def animate(i):
     
     ax.scatter(hist_p[i,:,0],hist_p[i,:,1],c="b")
     
-    ax.legend()
 ani = FuncAnimation(fig,animate,interval=0.1,frames=len(hist_p))
 plt.show()
     
